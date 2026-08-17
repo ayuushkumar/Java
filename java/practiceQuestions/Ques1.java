d1815affda5f7e19288fe8384cbad0d8d23e6898
Ayush <ayushkumar70505@gmail.com>
2026-08-17 23:06:00 +0530

    Q1-sum numbers

---
+++ java/practiceQuestions/Ques1.java
@@ -0,0 +1,16 @@
+package practiceQuestions;
+
+public class Ques1 {
+    public static void main(String[] args) {
+        int a = 4;
+        int b = 5;
+        int c = 6;
+        int sum = a + b + c;
+        System.out.println("Sum of the 3 numbers is :" + sum);
+
+    }
+
+    //Write a java program to add 3 numbers.
+
+
+}
