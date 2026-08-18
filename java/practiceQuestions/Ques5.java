5a9ae25a75981a292f125ac913b0b7464ecc481e
Ayush <ayushkumar70505@gmail.com>
2026-08-19 01:24:40 +0530

    Practice Question 5

---
+++ java/practiceQuestions/Ques5.java
@@ -0,0 +1,16 @@
+package practiceQuestions;
+import java.util.Scanner;
+
+public class Ques5 {
+
+/*
+    Q.write a java program to detect whether a number entered by the user
+    is integer or not.
+*/
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter to check: ");
+        System.out.println(sc.hasNextInt());
+    }
+}
