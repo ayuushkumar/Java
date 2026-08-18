34436cb68b2d30bcd1209c5b6d4f8b2fda0125e3
Ayush <ayushkumar70505@gmail.com>
2026-08-18 20:51:09 +0530

    Taking Input

--- java/javaCoding/J3_takingInput.java
+++ java/javaCoding/J3_takingInput.java
@@ -5,10 +5,12 @@ import java.util.Scanner;
 public class J3_takingInput {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        System.out.print("Enter your first number: ");
         int num1 = sc.nextInt();
+        System.out.print("Enter your second number: ");
         int num2 = sc.nextInt();
         int sum = num1 + num2;
-        System.out.println("sum = " + sum);
+        System.out.print("Sum of the numbers = " + sum);
 
     }
 }
