479074b230374f7fe4f9cfb6064d0cea459f2b58
Ayush <ayushkumar70505@gmail.com>
2026-08-18 21:07:37 +0530

    Input

--- java/javaCoding/J3_takingInput.java
+++ java/javaCoding/J3_takingInput.java
@@ -4,6 +4,7 @@ import java.util.Scanner;
 
 public class J3_takingInput {
     public static void main(String[] args) {
+        System.out.println("Taking input from the user");
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter your first number: ");
         int num1 = sc.nextInt();
@@ -12,6 +13,9 @@ public class J3_takingInput {
         int sum = num1 + num2;
         System.out.print("Sum of the numbers = " + sum);
 
+    }
+}
+
         /*
         Can also use :
         float a = sc.nextFloat();
@@ -19,7 +23,6 @@ public class J3_takingInput {
         byte b = sc.nextByte();
         long l = sc.nextLong();
         double d = sc.nextDouble();
+        boolean bool = sc.hasNextInt(); --> int,float,byte,short etc etc.
          */
 
-    }
-}
