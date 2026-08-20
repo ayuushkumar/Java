38dd27bd55278f78fec3b6bd8c90bf15de7301ba
Ayush <ayushkumar70505@gmail.com>
2026-08-20 23:13:51 +0530

    Ques7

---
+++ java/practiceQuestions/Ques7.java
@@ -0,0 +1,17 @@
+package practiceQuestions;
+
+public class Ques7 {
+    public static void main(String[] args) {
+
+// =======================================================
+// What will be the output of the following expression(x)
+//        int y = 7;
+//        int x = ++y * 8;
+//        value of x?
+// =======================================================
+
+        int y = 7;
+        int x = ++y * 8;
+        System.out.println(x); // 64
+    }
+}
