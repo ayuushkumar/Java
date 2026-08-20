ad52dad5453ae7f9ada7152fd578244ab88c540a
Ayush <ayushkumar70505@gmail.com>
2026-08-20 23:10:27 +0530

    Ques6

---
+++ java/practiceQuestions/Ques6.java
@@ -0,0 +1,21 @@
+package practiceQuestions;
+
+public class Ques6 {
+    public static void main(String[] args) {
+
+//==========================================================
+// Try increment and decrement operator on a java program.
+//==========================================================
+
+        int i = 6;
+        System.out.println(i++); // Prints 6 becomes 7
+        System.out.println(i--); // 7 --> 6
+        System.out.println(--i); // 5
+        System.out.println(++i); // 6
+        int j = 7;
+        System.out.println(j++); // Prints 7 becomes 8
+        System.out.println(j--); // 8 --> 7
+        System.out.println(--j); // 6
+        System.out.println(++j); // 7
+    }
+}
