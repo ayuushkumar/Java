194163d101cb7d2f698553bbb170a937561f387d
Ayush <ayushkumar70505@gmail.com>
2026-08-20 23:17:48 +0530

    Ques1

--- java/practiceQuestions/Ques1.java
+++ java/practiceQuestions/Ques1.java
@@ -2,11 +2,9 @@ package practiceQuestions;
 
 public class Ques1 {
 
-/*
-=======================================================================================
-    Q.Write a java program to add 3 numbers.
-=======================================================================================
- */
+// ===================================================================
+//   Q.write a program to add 3 numbers.
+// ===================================================================
 
     public static void main(String[] args) {
         int a = 4;
@@ -16,6 +14,4 @@ public class Ques1 {
         System.out.println("Sum of the 3 numbers is :" + sum);
 
     }
-
-
 }
