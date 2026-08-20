c5ef89e94ddf8a08b1beddabbc0847f375a12d6e
Ayush <ayushkumar70505@gmail.com>
2026-08-20 23:20:18 +0530

    Ques5

--- java/practiceQuestions/Ques5.java
+++ java/practiceQuestions/Ques5.java
@@ -3,12 +3,10 @@ import java.util.Scanner;
 
 public class Ques5 {
 
-/*
-=======================================================================================
-    Q.write a java program to detect whether a number entered by the user
-    is integer or not.
-=======================================================================================
-*/
+// ==================================================================================
+//    Q.write a java program to detect whether a number entered by the user
+//    is integer or not.
+// ==================================================================================
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
