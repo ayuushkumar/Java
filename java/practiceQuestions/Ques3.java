0ae05fd307e20bc628287e9125bb485ba4f07315
Ayush <ayushkumar70505@gmail.com>
2026-08-20 23:19:00 +0530

    Ques3

--- java/practiceQuestions/Ques3.java
+++ java/practiceQuestions/Ques3.java
@@ -2,12 +2,12 @@ package practiceQuestions;
 import java.util.Scanner;
 
 public class Ques3 {
-/*
-=======================================================================================
-    Q.Write a java program which asks the user to enter his/her name
-    and greets them with "Hello <name>, have a good day!" text.
-=======================================================================================
-*/
+
+// ==================================================================================
+//    Q.Write a java program which asks the user to enter his/her name
+//    and greets them with "Hello <name>, have a good day!" text.
+// ==================================================================================
+
 
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
