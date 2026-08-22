563931bafc2a0e069dccf5478f038d98a0b16da3
Ayush <ayushkumar70505@gmail.com>
2026-08-22 23:38:33 +0530

    String - Ques1

---
+++ java/practiceQuestions/Ques10.java
@@ -0,0 +1,27 @@
+package practiceQuestions;
+
+public class Ques10 {
+    public static void main(String[] args) {
+
+// ===========================================================
+// Q. Write a java program to convert a string to lower case.
+// ===========================================================
+
+        String java = "LANGUAGE";
+//      System.out.println(java.toLowerCase());
+        java = java.toLowerCase();
+        System.out.println(java);
+
+// ===========================================================
+// Q. Write a java program to replace spaces with underscore.
+// ===========================================================
+
+        String str = "J A V A";
+//      System.out.println(str.replace(" ","_"));
+        str = str.replace(" ","_");
+        System.out.println(str);
+
+
+
+    }
+}
