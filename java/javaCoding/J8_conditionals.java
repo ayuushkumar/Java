96f3dcf607c857a9b0cefa90c5992e4c74c70887
Ayush <ayushkumar70505@gmail.com>
2026-08-23 16:40:13 +0530

    Conditionals

---
+++ java/javaCoding/J8_conditionals.java
@@ -0,0 +1,27 @@
+package javaCoding;
+
+public class J8_conditionals {
+    public static void main(String[] args) {
+        int age = 19;
+//      boolean bool = (age >= 18);
+//      if(bool){
+        if(age >= 18){
+            System.out.println("Yes, You can drive");
+//  else block is optional.
+        }else{
+            System.out.println("No, Not yet");
+        }
+
+// ============ RELATIONAL OPERATOR ==============
+//      == --> equals
+//      >= --> greater then or equal to
+//      >  --> greater then
+//      <  --> less then
+//      <= --> less then or equal to
+//      != --> not equals
+
+// ======= NOTE =========
+// '=' is used for assignment where as '==' is used for equality check.
+
+    }
+}
