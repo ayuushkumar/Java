e62b7e6cac1d69a926b2e6d2b5768f47798f243b
Ayush <ayushkumar70505@gmail.com>
2026-08-26 23:37:26 +0530

    While loop

---
+++ java/javaCoding/J11_loops.java
@@ -0,0 +1,20 @@
+package javaCoding;
+
+public class J11_loops {
+    public static void main(String[] args) {
+
+//  While loop
+
+        //while (condition)
+        //{
+        // Statements    -> This keeps executing as long as the condition is true.
+        //}
+
+        System.out.println("Using While Loop");
+        int i = 1;
+        while(i <= 5){
+            System.out.println(i);
+            i++;
+        }
+    }
+}
