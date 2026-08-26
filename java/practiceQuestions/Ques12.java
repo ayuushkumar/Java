aca8e49d054295073c2b25ac7e3d89bf086ad14a
Ayush <ayushkumar70505@gmail.com>
2026-08-26 23:57:00 +0530

    Loops - Ques1

---
+++ java/practiceQuestions/Ques12.java
@@ -0,0 +1,16 @@
+package practiceQuestions;
+
+public class Ques12 {
+    public static void main(String[] args) {
+
+// =====================================================================
+//  Q.Write a java program to print natural numbers from 100 to 200.
+// =====================================================================
+
+        int num = 100;
+        while(num <= 200){
+            System.out.println(num);
+            num++;
+        }
+    }
+}
