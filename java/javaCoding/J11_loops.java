e5714c08abf6c5b1c8c4c0d23a21af7c9bb7e4f0
Ayush <ayushkumar70505@gmail.com>
2026-08-27 23:43:28 +0530

    Do While loop

--- java/javaCoding/J11_loops.java
+++ java/javaCoding/J11_loops.java
@@ -17,12 +17,27 @@ public class J11_loops {
             i++;
         }
 
+//===== WARNING! INFINITE WHILE LOOP. =====
 //  If the Condition never becomes false, the while loop keeps getting executed.
 //  such a loop is known as an infinite loop.
 
-        System.out.println("This is an infinite loop");
-        while(true){
-            System.out.println("Infinite loop");
-        }
+//        System.out.println("This is an infinite loop");
+//        while(true){
+//            System.out.println("Infinite loop");
+//        }
+
+
+//  Do While loop
+
+        // Execute the code once & then check the condition.
+        //do{
+        // Statements
+        //} while (condition);
+
+        int a = 0;
+        do{
+            System.out.println(a);
+            a++;
+        }while(a<5);
     }
 }
