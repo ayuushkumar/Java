d907d3b83d51c85a64237915051e194173204148
Ayush <ayushkumar70505@gmail.com>
2026-08-27 23:48:01 +0530

    Loops - Ques2

---
+++ java/practiceQuestions/Ques13.java
@@ -0,0 +1,18 @@
+package practiceQuestions;
+
+public class Ques13 {
+    public static void main(String[] args) {
+
+// =====================================================================
+//  Q.Write a java program to print first n natural numbers
+//  using do-while loop.
+// =====================================================================
+
+        int n = 0;
+        do{
+            System.out.println(n);
+            n++;
+        }while(n<=45);
+
+    }
+}
