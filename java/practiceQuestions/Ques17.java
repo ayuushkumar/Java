e657f2ed45c03d149d24ab9cc0e9e0a4635093b4
Ayush <ayushkumar70505@gmail.com>
2026-08-30 17:24:13 +0530

    Arrays - Ques1-2

--- java/practiceQuestions/Ques17.java
+++ java/practiceQuestions/Ques17.java
@@ -13,5 +13,25 @@ public class Ques17 {
             sum += addition;
         }
         System.out.println("The sum is: " + sum);
+
+// =============================================================
+//  Q.Write a program to find out whether a given number is
+//  present in an array or not.
+// =============================================================
+
+        float[] marks2= {89.5f, 67.7f, 90.9f, 78.8f, 88.9f};
+        float num = 67.7f;
+        boolean isInArray = false;
+        for(float find: marks2){
+            if(num==find){
+                isInArray = true;
+                break;
+            }
+        }
+        if(isInArray){
+            System.out.println("The number is in the array");
+        }else{
+            System.out.println("The number is not in the array");
+        }
     }
 }
