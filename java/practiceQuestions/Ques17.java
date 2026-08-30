fb0de05b39a912a5b95d663877be471a718d088d
Ayush <ayushkumar70505@gmail.com>
2026-08-30 17:32:40 +0530

    Arrays - Ques1-4

--- java/practiceQuestions/Ques17.java
+++ java/practiceQuestions/Ques17.java
@@ -46,5 +46,20 @@ public class Ques17 {
         }
         System.out.println("\nThe average of the class is: " + sum/marks3.length);
 
+// =============================================================
+//  Q.Create a java program to add two matrices of size 2X3.
+// =============================================================
+
+        int[][] mat1 = {{2,3,4},{5,6,7}};
+        int[][] mat2 = {{1,2,3},{4,5,6}};
+        int[][] result = {{0,0,0},{0,0,0}};
+        System.out.print("\n");
+        for(int i=0; i<mat1.length;i++){
+            for(int j=0; j<mat1[i].length; j++){
+                result [i][j] = mat1[i][j] + mat2[i][j];
+                System.out.print(result[i][j] + " ");
+            }
+            System.out.println();
+        }
     }
 }
