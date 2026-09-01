841df37a504501b9ac20e9a19dc6e33b8c5affaa
Ayush <ayushkumar70505@gmail.com>
2026-09-02 00:09:43 +0530

    Method - Calling a Method

--- java/javaCoding/J13_1_methods.java
+++ java/javaCoding/J13_1_methods.java
@@ -34,10 +34,32 @@ public class J13_1_methods {
 //        }else{
 //            c = (a+b)*5;
 //        }
+//        System.out.println(c);
 
         int a1 = 6;
         int b1 = 7;
         int c1 = logic(a1,b1);
         System.out.println(c1);
+
+// ====================================================
+//  Method invocation using Object creation --
+
+//  int logic(int x, int y){
+//        int z;
+//        if(x<y){
+//            z = x+y;
+//        }else{
+//            z = (x+y)*5;
+//        }
+//        return z;
+//    }
+//        public static void main(String[] args) {
+
+//         int a = 7;
+//         int b = 6;
+//         J13_1_methods obj = new J13_1_methods();
+//         int c = obj.logic(a, b);
+//         System.out.println(c);
+// ====================================================
     }
 }
