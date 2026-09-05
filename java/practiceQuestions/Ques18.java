8bdb19fe0886f951bfa7ec02fc1749e6ceb85a70
Ayush <ayushkumar70505@gmail.com>
2026-09-05 18:44:15 +0530

    Methods - Ques1-2

--- java/practiceQuestions/Ques18.java
+++ java/practiceQuestions/Ques18.java
@@ -1,5 +1,7 @@
 package practiceQuestions;
 
+import java.sql.SQLOutput;
+
 public class Ques18 {
 
 // ============================================================
@@ -12,9 +14,29 @@ public class Ques18 {
             System.out.printf("%d X %d = %d\n",n,i,n*i);
         }
     }
+// ============================================================
+//  Q. Write a java program to print Fibonacci series
+//  (recursion must be used)
+// ============================================================
+
+    static int Fibonacci(int f){
+        if(f==0 || f==1){
+            return f;
+        }else{
+            return Fibonacci(f-1)+Fibonacci(f-2);
+        }
+    }
+
     public static void main(String[] args) {
 
 //  Problem 1
         multiplication(7);
+
+// Problem 2
+        int x = 5;
+        System.out.println("\nFibonacci number "+x+" is: ");
+        for(int i=0;i<=x;i++){
+            System.out.print(Fibonacci(i)+" ");
+        }
     }
 }
