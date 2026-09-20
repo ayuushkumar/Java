99ec8eea14e5a28bcbde9cc6bed9e21d21cbc643
Ayush <ayushkumar70505@gmail.com>
2026-09-20 22:43:11 +0530

    AbstractClasses & Interfaces - Ques1-2

--- java/practiceQuestions/Ques22.java
+++ java/practiceQuestions/Ques22.java
@@ -19,13 +19,35 @@ class BallPen extends Pen{
     }
 }
 
+// =========================================================
+//  Q. Use the Pen Class from Q1 to create a Concrete class
+//  Fountain Pen with additional method changeNib()
+// =========================================================
+class FountainPen extends Pen{
+    void write(){
+        System.out.println("Write with Fountain pen");
+    }
+    void refill(){
+        System.out.println("Refill Fountain pen");
+    }
+    void changeNib(){
+        System.out.println("Changing the Nib of Fountain pen");
+    }
+}
+
 public class Ques22 {
     public static void main(String[] args) {
 
-// Problem 1
+//  Problem 1
         BallPen pen = new BallPen();
         pen.refill();
         pen.write();
+
+//  Problem 2
+        FountainPen fp = new FountainPen();
+        fp.changeNib();
+        fp.write();
+        fp.refill();
 
     }
 }
